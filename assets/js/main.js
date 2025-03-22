8ffa7789dd83cdc9a77a6193ccee8c39c0c9c31f
Alexis Lopez Fregoso <alexislopezfregoso@MacBook-Pro-de-Alexis.local>
2025-03-22 16:09:34 -0600

    Primera versión de mi página web

---
+++ assets/js/main.js
@@ -0,0 +1,76 @@
+/*Alexis Lopez Fregoso*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+		
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			mode: 'fade',
+			noOpenerFade: true,
+			hoverDelay: 150,
+			hideDelay: 350
+		});
+
+//carrusel
+function changeImage(imageSrc) {
+    document.getElementById('current-image').src = imageSrc;
+}
+	// Nav.
+
+		// boton de compra
+	
+	document.querySelectorAll('.btn-comprar').forEach(button => {
+		button.addEventListener('click', function() {
+			const url = this.getAttribute('data-url'); // Obtiene la URL del botón
+			window.location.href = url; // Redirige a la página del producto
+		});
+	});
+	
+
+		// Title Bar.
+			$(
+				'<div id="titleBar">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+})(jQuery);
